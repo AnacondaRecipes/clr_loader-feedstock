8341830a9101a4e19a1881d957dd3e215189c57e
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2023-01-25 23:35:26 -0600

    fix linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,8 @@
 # Install the latest dotnet-core and SDK using dotnet-install scripts.
 mkdir -p $BUILD_PREFIX
 curl -L -o $BUILD_PREFIX/dotnet-install.sh https://dot.net/v1/dotnet-install.sh && chmod +x $BUILD_PREFIX/dotnet-install.sh
-./$BUILD_PREFIX/dotnet-install.sh -install_dir $BUILD_PREFIX/dotnet -version latest
-export PATH=$BUILD_PREFIX/dotnet;$PATH
+source $BUILD_PREFIX/dotnet-install.sh --install-dir $BUILD_PREFIX/dotnet --version latest
+export PATH="$BUILD_PREFIX/dotnet:$PATH"
 
 # Now install the package.
 $PYTHON -m pip install . -vv
