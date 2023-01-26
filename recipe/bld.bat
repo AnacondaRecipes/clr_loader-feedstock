374811c2c0eb24f4a82adcf8069c142b61593000
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2023-01-25 22:44:32 -0600

    initial build

---
+++ recipe/bld.bat
@@ -0,0 +1,8 @@
+REM Install the latest dotnet-core and SDK using dotnet-install scripts.
+mkdir %BUILD_PREFIX% 2> NUL
+curl -L -o %BUILD_PREFIX%\dotnet-install.ps1 https://dot.net/v1/dotnet-install.ps1
+powershell %BUILD_PREFIX%\dotnet-install.ps1 -InstallDir %BUILD_PREFIX%\dotnet -Version latest
+set PATH=%BUILD_PREFIX%\dotnet;%PATH%
+
+REM Now install the package
+%PYTHON% -m pip install . -vv
