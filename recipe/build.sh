359a42dd24d5fb9a3e4786594189695d0fa32dde
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2023-01-27 12:46:53 -0600

    fix icu failure on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ mkdir -p $BUILD_PREFIX
 curl -L -o $BUILD_PREFIX/dotnet-install.sh https://dot.net/v1/dotnet-install.sh && chmod +x $BUILD_PREFIX/dotnet-install.sh
 source $BUILD_PREFIX/dotnet-install.sh --install-dir $BUILD_PREFIX/dotnet --version latest
 export PATH="$BUILD_PREFIX/dotnet:$PATH"
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
 # Now install the package.
 $PYTHON -m pip install . --no-deps -vv
