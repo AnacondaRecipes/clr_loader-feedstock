9099c2b3a67ac9131e4860a56d0dcc4a7b1610de
Andrii-Osipov <aosipov@anaconda.com>
2026-08-31 00:05:05 -0700

    clr_loader 0.3.1 (#9)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,10 +2,13 @@ REM Install the latest dotnet-core and SDK using dotnet-install scripts.
 mkdir %BUILD_PREFIX% 2> NUL
 if exist %BUILD_PREFIX%\dotnet ( rmdir %BUILD_PREFIX%\dotnet /s /q )
 curl -L -o %BUILD_PREFIX%\dotnet-install.ps1 https://dot.net/v1/dotnet-install.ps1
+if errorlevel 1 exit /b 1
 powershell %BUILD_PREFIX%\dotnet-install.ps1 -InstallDir %BUILD_PREFIX%\dotnet -Version latest
+if errorlevel 1 exit /b 1
 set PATH=%BUILD_PREFIX%\dotnet;%PATH%
 
 REM Now install the package
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
+if errorlevel 1 exit /b 1
 REM Remember to shutdown the build server so that future builds aren't affected.
 dotnet build-server shutdown
